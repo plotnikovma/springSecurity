f8e8708efcc6a0889f0536a7b4e597493f8b3d81
Максим Плотников <mplotnikov@naumen.ru>
2021-09-11 10:53:14 +0500

    Step2. Добавление Spring Security и аутентификация с помощью Basic Auth

--- springSecurity/src/main/java/ru/home/springsecurityproject/model/Employee.java
+++ springSecurity/src/main/java/ru/home/springsecurityproject/model/Employee.java
@@ -1,19 +1,55 @@
 package ru.home.springsecurityproject.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 /**
  * Модель пользователя системы
  *
  * @author mplotnikov
  * @since 11.09.2021
  */
-@Data
-@AllArgsConstructor
 public class Employee
 {
     private Long id;
     private String firstName;
     private String lastName;
+
+    public Employee()
+    {
+    }
+
+    public Employee(Long id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 }
